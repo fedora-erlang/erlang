93bda3251385daf0658f836217524273cf4be3fc
Peter Lemenkov <lemenkov@gmail.com>
2016-07-14 17:51:16 +0300

    Respect -proto_dist switch while connection to EPMD
    Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

--- lib/kernel/src/inet_tcp_dist.erl
+++ lib/kernel/src/inet_tcp_dist.erl
@@ -73,7 +73,7 @@ gen_listen(Driver, Name) ->
 	{ok, Socket} ->
 	    TcpAddress = get_tcp_address(Driver, Socket),
 	    {_,Port} = TcpAddress#net_address.address,
-	    case erl_epmd:register_node(Name, Port) of
+	    case erl_epmd:register_node(Name, Port, Driver) of
 		{ok, Creation} ->
 		    {ok, {Socket, TcpAddress, Creation}};
 		Error ->
