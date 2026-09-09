erlang/otp

ssl: Use digraph to ensure robust cert chain building.
Handling of extraneous and unordered certs could cause
cycles causing a possible DoS attack. Also add an
in depth length check of built chains.

lib/ssl/test/ssl_cert_SUITE.erl
@@ -123,6 +123,12 @@
          cross_signed_chain/1,
          expired_root_with_cross_signed_root/0,
          expired_root_with_cross_signed_root/1,
+         malicious_cycle_in_peer_chain/0,
+         malicious_cycle_in_peer_chain/1,
+         max_chain_depth_buildup/0,
+         max_chain_depth_buildup/1,
+         duplicate_issuer_in_trust_store/0,
+         duplicate_issuer_in_trust_store/1,
          key_auth_ext_sign_only/0,
          key_auth_ext_sign_only/1,
          hello_retry_request/0,
@@ -247,7 +253,10 @@ rsa_tests() ->
    [
     longer_chain,
     cross_signed_chain,
-    expired_root_with_cross_signed_root
+    expired_root_with_cross_signed_root,
+    malicious_cycle_in_peer_chain,
+    max_chain_depth_buildup,
+    duplicate_issuer_in_trust_store
    ].
 
 tls_1_3_rsa_tests() ->
@@ -1264,6 +1273,120 @@ expired_root_with_cross_signed_root(Config) when is_list(Config) ->
                              {cacerts, [AltCrossRoot | ClientCas0]} |
                              proplists:delete(cacerts, ClientOpts)],
                             ServerOpts, Config).
+
+%%--------------------------------------------------------------------
+malicious_cycle_in_peer_chain() ->
+    [{doc, "A malicious client sends an unordered chain to a server. "
+      "The server processes it through unorded_or_extraneous/2 which "
+      "uses an acyclic digraph. Verify that path construction from "
+      "the unordered chain terminates and does not loop."}].
+malicious_cycle_in_peer_chain(Config) when is_list(Config) ->
+    Key1 = ssl_test_lib:hardcode_rsa_key(1),
+    Key2 = ssl_test_lib:hardcode_rsa_key(2),
+    Key3 = ssl_test_lib:hardcode_rsa_key(3),
+    Key4 = ssl_test_lib:hardcode_rsa_key(4),
+    Key5 = ssl_test_lib:hardcode_rsa_key(5),
+
+    %% Client chain with cross-key intermediates
+    #{client_config := ClientOpts0} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, Key4}],
+                              peer => [{key, Key5}]},
+            client_chain => #{root => [{key, Key1}],
+                              intermediates => [[{key, Key2}], [{key, Key1}]],
+                              peer => [{key, Key3}]}}),
+
+    %% Build the client's ordered chain
+    ClientCert = proplists:get_value(cert, ClientOpts0),
+    ClientCAs = proplists:get_value(cacerts, ClientOpts0),
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, ClientCAs}),
+    {ok, _, [Peer, CA1, CA2, Root]} =
+        ssl_certificate:certificate_chain(ClientCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+
+    %% Shuffle chain so it's unordered — triggers unorded_or_extraneous
+    MaliciousChain = [Peer, Root, CA2, CA1],
+    CertRecs = [#cert{der=D, otp=public_key:pkix_decode_cert(D, otp)}
+                || D <- MaliciousChain],
+
+    %% Call trusted_cert_and_paths directly — this is the code path
+    %% that would hang without the digraph fix
+    %% Use empty trust store so no path can be validated
+    Result = ssl_certificate:trusted_cert_and_paths(
+               CertRecs, ets:new(foo, []), {extracted, []},
+               fun(_) -> unknown_ca end),
+
+    %% Must return (not hang) with unknown_ca for all paths
+    lists:foreach(fun({unknown_ca, _}) -> ok;
+                     ({#cert{}, _}) -> ok
+                  end, Result).
+
+%%--------------------------------------------------------------------
+max_chain_depth_buildup() ->
+    [{doc, "Chain building stops at MAX_CHAIN (12) even when the trust "
+      "store contains a longer valid chain. Guards against resource "
+      "exhaustion from very deep chains."}].
+max_chain_depth_buildup(Config) when is_list(Config) ->
+    %% Create chain with 15 intermediates — exceeds MAX_CHAIN (12)
+    Keys = [ssl_test_lib:hardcode_rsa_key((N rem 6) + 1)
+            || N <- lists:seq(1, 17)],
+    [RootKey, PeerKey | CAKeys] = Keys,
+    IntermediateOpts = [[{key, K}] || K <- CAKeys],
+
+    #{server_config := ServerOpts} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, RootKey}],
+                              intermediates => IntermediateOpts,
+                              peer => [{key, PeerKey}]},
+            client_chain => #{root => [{key, RootKey}],
+                              peer => [{key, PeerKey}]}}),
+
+    SCert = proplists:get_value(cert, ServerOpts),
+    SCerts = proplists:get_value(cacerts, ServerOpts),
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, SCerts}),
+
+    %% Build chain — must terminate and respect the MAX_CHAIN limit
+    {ok, _Root, Chain} =
+        ssl_certificate:certificate_chain(SCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+    %% MAX_CHAIN is 12: chain must not exceed that
+    true = (length(Chain) =< 12).
+
+%%--------------------------------------------------------------------
+duplicate_issuer_in_trust_store() ->
+    [{doc, "Trust store lookup returns a cert already in the chain. "
+      "The duplicate check in do_certificate_chain must detect this "
+      "and terminate instead of looping. Tests the DER-based "
+      "duplicate guard added in OTP-20245."}].
+duplicate_issuer_in_trust_store(Config) when is_list(Config) ->
+    Key1 = ssl_test_lib:hardcode_rsa_key(1),
+    Key2 = ssl_test_lib:hardcode_rsa_key(2),
+    Key3 = ssl_test_lib:hardcode_rsa_key(3),
+
+    #{server_config := ServerOpts0} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, Key1}],
+                              intermediates => [[{key, Key2}]],
+                              peer => [{key, Key3}]},
+            client_chain => #{root => [{key, Key1}],
+                              peer => [{key, Key3}]}}),
+
+    SCert = proplists:get_value(cert, ServerOpts0),
+    SCerts = proplists:get_value(cacerts, ServerOpts0),
+
+    %% Add peer cert to trust store — creates potential for
+    %% lookup_trusted_cert to return a cert already in chain
+    PoisonedCAs = [SCert | SCerts],
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, PoisonedCAs}),
+
+    %% Must terminate (not hang) and produce a valid chain
+    {ok, _Root, Chain} =
+        ssl_certificate:certificate_chain(SCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+    %% No duplicates in result
+    true = (length(Chain) =:= length(lists:usort(Chain))),
+    %% Reasonable length (normal: peer + CA + root = 3)
+    true = (length(Chain) =< 4).
 
 %%--------------------------------------------------------------------
 %% TLS 1.3 Test cases  -----------------------------------------------

fedora-erlang/erlang

Backport note (OTP 23): ssl_certificate:trusted_cert_and_paths/4 takes
DER binaries and returns decoded #'OTPCertificate'{} roots on this
branch, so the malicious_cycle_in_peer_chain test is adjusted
accordingly. subject/1 lost its only caller and is removed as well.
(cherry picked from commit 0307bff2c72b685c6bd952daaac6bd661c247d62)
Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>
Signed-off-by: Peter Lemenkov <lemenkov@gmail.com>

lib/ssl/test/ssl_cert_SUITE.erl
@@ -102,6 +102,12 @@
          cross_signed_chain/1,
          expired_root_with_cross_signed_root/0,
          expired_root_with_cross_signed_root/1,
+         malicious_cycle_in_peer_chain/0,
+         malicious_cycle_in_peer_chain/1,
+         max_chain_depth_buildup/0,
+         max_chain_depth_buildup/1,
+         duplicate_issuer_in_trust_store/0,
+         duplicate_issuer_in_trust_store/1,
          key_auth_ext_sign_only/0,
          key_auth_ext_sign_only/1,
          hello_retry_request/0,
@@ -198,7 +204,10 @@ rsa_tests() ->
    [
     longer_chain,
     cross_signed_chain,
-    expired_root_with_cross_signed_root
+    expired_root_with_cross_signed_root,
+    malicious_cycle_in_peer_chain,
+    max_chain_depth_buildup,
+    duplicate_issuer_in_trust_store
    ].
 
 tls_1_3_rsa_tests() ->
@@ -1014,6 +1023,118 @@ expired_root_with_cross_signed_root(Config) when is_list(Config) ->
     ssl_test_lib:basic_test([{verify, verify_peer},
                              {cacerts, [AltCrossRoot | ClientCas0]} | proplists:delete(cacerts, ClientOpts)],
                             ServerOpts, Config).
+
+%%--------------------------------------------------------------------
+malicious_cycle_in_peer_chain() ->
+    [{doc, "A malicious client sends an unordered chain to a server. "
+      "The server processes it through unorded_or_extraneous/1 which "
+      "uses an acyclic digraph. Verify that path construction from "
+      "the unordered chain terminates and does not loop."}].
+malicious_cycle_in_peer_chain(Config) when is_list(Config) ->
+    Key1 = ssl_test_lib:hardcode_rsa_key(1),
+    Key2 = ssl_test_lib:hardcode_rsa_key(2),
+    Key3 = ssl_test_lib:hardcode_rsa_key(3),
+    Key4 = ssl_test_lib:hardcode_rsa_key(4),
+    Key5 = ssl_test_lib:hardcode_rsa_key(5),
+
+    %% Client chain with cross-key intermediates
+    #{client_config := ClientOpts0} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, Key4}],
+                              peer => [{key, Key5}]},
+            client_chain => #{root => [{key, Key1}],
+                              intermediates => [[{key, Key2}], [{key, Key1}]],
+                              peer => [{key, Key3}]}}),
+
+    %% Build the client's ordered chain
+    ClientCert = proplists:get_value(cert, ClientOpts0),
+    ClientCAs = proplists:get_value(cacerts, ClientOpts0),
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, ClientCAs}),
+    {ok, _, [Peer, CA1, CA2, Root]} =
+        ssl_certificate:certificate_chain(ClientCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+
+    %% Shuffle chain so it's unordered — triggers unorded_or_extraneous
+    MaliciousChain = [Peer, Root, CA2, CA1],
+
+    %% Call trusted_cert_and_paths directly — this is the code path
+    %% that would hang without the digraph fix
+    %% Use empty trust store so no path can be validated
+    Result = ssl_certificate:trusted_cert_and_paths(
+               MaliciousChain, ets:new(foo, []), {extracted, []},
+               fun(_) -> unknown_ca end),
+
+    %% Must return (not hang) with unknown_ca for all paths
+    lists:foreach(fun({unknown_ca, _}) -> ok;
+                     ({#'OTPCertificate'{}, _}) -> ok
+                  end, Result).
+
+%%--------------------------------------------------------------------
+max_chain_depth_buildup() ->
+    [{doc, "Chain building stops at MAX_CHAIN (12) even when the trust "
+      "store contains a longer valid chain. Guards against resource "
+      "exhaustion from very deep chains."}].
+max_chain_depth_buildup(Config) when is_list(Config) ->
+    %% Create chain with 15 intermediates — exceeds MAX_CHAIN (12)
+    Keys = [ssl_test_lib:hardcode_rsa_key((N rem 6) + 1)
+            || N <- lists:seq(1, 17)],
+    [RootKey, PeerKey | CAKeys] = Keys,
+    IntermediateOpts = [[{key, K}] || K <- CAKeys],
+
+    #{server_config := ServerOpts} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, RootKey}],
+                              intermediates => IntermediateOpts,
+                              peer => [{key, PeerKey}]},
+            client_chain => #{root => [{key, RootKey}],
+                              peer => [{key, PeerKey}]}}),
+
+    SCert = proplists:get_value(cert, ServerOpts),
+    SCerts = proplists:get_value(cacerts, ServerOpts),
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, SCerts}),
+
+    %% Build chain — must terminate and respect the MAX_CHAIN limit
+    {ok, _Root, Chain} =
+        ssl_certificate:certificate_chain(SCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+    %% MAX_CHAIN is 12: chain must not exceed that
+    true = (length(Chain) =< 12).
+
+%%--------------------------------------------------------------------
+duplicate_issuer_in_trust_store() ->
+    [{doc, "Trust store lookup returns a cert already in the chain. "
+      "The duplicate check in do_certificate_chain must detect this "
+      "and terminate instead of looping. Tests the DER-based "
+      "duplicate guard added in OTP-20245."}].
+duplicate_issuer_in_trust_store(Config) when is_list(Config) ->
+    Key1 = ssl_test_lib:hardcode_rsa_key(1),
+    Key2 = ssl_test_lib:hardcode_rsa_key(2),
+    Key3 = ssl_test_lib:hardcode_rsa_key(3),
+
+    #{server_config := ServerOpts0} =
+        public_key:pkix_test_data(
+          #{server_chain => #{root => [{key, Key1}],
+                              intermediates => [[{key, Key2}]],
+                              peer => [{key, Key3}]},
+            client_chain => #{root => [{key, Key1}],
+                              peer => [{key, Key3}]}}),
+
+    SCert = proplists:get_value(cert, ServerOpts0),
+    SCerts = proplists:get_value(cacerts, ServerOpts0),
+
+    %% Add peer cert to trust store — creates potential for
+    %% lookup_trusted_cert to return a cert already in chain
+    PoisonedCAs = [SCert | SCerts],
+    {ok, ExtractedCAs} = ssl_pkix_db:extract_trusted_certs({der, PoisonedCAs}),
+
+    %% Must terminate (not hang) and produce a valid chain
+    {ok, _Root, Chain} =
+        ssl_certificate:certificate_chain(SCert, ets:new(foo, []),
+                                          ExtractedCAs, [], encoded),
+    %% No duplicates in result
+    true = (length(Chain) =:= length(lists:usort(Chain))),
+    %% Reasonable length (normal: peer + CA + root = 3)
+    true = (length(Chain) =< 4).
 
 %%--------------------------------------------------------------------
 %% TLS 1.3 Test cases  -----------------------------------------------